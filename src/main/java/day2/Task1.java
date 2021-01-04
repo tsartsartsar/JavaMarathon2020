454845ce3f9d656f5466c84a4bb91e583cc55f89
tsartsartsar <a.rahmatullin1999@gmail.com>
2021-01-04 20:35:32 +0400

    day2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,18 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int stage = Integer.parseInt(scanner.nextLine());
+        if ((stage >= 1) && (stage <= 4))
+            System.out.println("Малоэтажный дом");
+        else if ((stage >= 5) && (stage <= 8))
+            System.out.println("Среднеэтажный дом");
+        else if (stage >= 9)
+            System.out.println("Многоэтажный дом");
+        else
+            System.out.println("Ошибка ввода");
     }
 }
