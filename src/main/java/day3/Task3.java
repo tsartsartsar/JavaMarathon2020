f6682de113ac753f9479fb600484c7ce9ecf6c50
tsartsartsar <a.rahmatullin1999@gmail.com>
2021-01-04 21:38:20 +0400

    day3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int x = 0;
+        while (x < 5){
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            x++;
+            if (b == 0){
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(a / b);
+        }
     }
 }
