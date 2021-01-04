8b102a6dde6921b88d0753108e14867c6692ba52
tsartsartsar <a.rahmatullin1999@gmail.com>
2021-01-04 19:17:57 +0400

    day1

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int x = 0;
+        while (x < 10){
+            x++;
+            System.out.print("JAVA ");
+        }
     }
 }
