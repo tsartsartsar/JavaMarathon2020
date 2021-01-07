80d1ff3e331f81d293414cf4d9548651024b5b5c
tsartsartsar <a.rahmatullin1999@gmail.com>
2021-01-08 00:10:19 +0400

    day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,10 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setYear(2000);
+        car.setColor("red");
+        car.setModel("toyota");
+        System.out.println(car.getYear() + ", " + car.getColor() + ", " + car.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,7 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Motorbike motorbike = new Motorbike(2005, "green", "suzuki");
+        System.out.println(motorbike.getYear() + ", " + motorbike.getColor() + ", " + motorbike.getModel());
     }
 }
